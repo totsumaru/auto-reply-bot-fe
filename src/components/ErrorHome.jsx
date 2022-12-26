8ae1e05aedaad1e166b418d31253af3b3f7b255f
totsuka <totsuka@totsukanoMacBook-Air.local>
2022-12-26 17:29:25 +0900

    デバッグ用関数を削除

--- src/components/ErrorHome.jsx
+++ src/components/ErrorHome.jsx
@@ -9,7 +9,6 @@ export const ErrorHome = () => {
 
   // DiscordのログインURLを作成します
   const createDiscordLoginURL = () => {
-    console.log("ENV FE:", process.env.REACT_APP_FE_ROOT_URL)
     const redirectURL = `${process.env.REACT_APP_FE_ROOT_URL}?id=${serverID}`
     const encodedRedirectURL = encodeURI(redirectURL)
     return `https://discord.com/api/oauth2/authorize?client_id=1055348253614419989&redirect_uri=${encodedRedirectURL}&response_type=code&scope=identify`
